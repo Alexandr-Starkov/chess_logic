f5151ca1843dec095d7c48fcc1fda06843f1c022
Alexandr-Starkov <Starkoff55@gmail.com>
2024-05-02 17:22:42 +0600

    added classes and methods to them

--- src/main.py
+++ src/main.py
@@ -1,26 +1,111 @@
+from typing import List
+
+
+class Figure:
+    def move(self):
+        pass
+
+    def cut_an_enemy_figure(self):
+        pass
+
+
+class Pawn(Figure):
+    def __init__(self):
+        self.identity = 'p'
+
+    @staticmethod
+    def create_pawns():
+        pawns = []
+
+        for i in range(1, 17):
+            p = Pawn()
+            p.id = i
+            pawns.append(p)
+
+        return pawns
+
+
+class Knight(Figure):
+    @staticmethod
+    def create_knights(self):
+        pass
+
+
+class Bishops(Figure):
+    @staticmethod
+    def create_bishops(self):
+        pass
+
+
+class Rook(Figure):
+    @staticmethod
+    def create_rooks(self):
+        pass
+
+
+class Queen(Figure):
+    @staticmethod
+    def create_queen(self):
+        pass
+
+
+class King(Figure):
+    @staticmethod
+    def create_king():
+        pass
+
 
 class Board:
-    def __init__(self, width=8, heigth=8):
-        self.width = width
-        self.height = heigth
+    def __init__(self):
+        self.width = 8
+        self.height = 8
         self.board = []
 
     def create_board(self):
         '''Board creation'''
         for i in range(self.height):
+            # figures of the first two lines
             if i < 2:
-                self.board.append(['x'] * self.width)
+                self.board.append([' '] * self.width)
+            # figures of the last two lines
             elif i in (self.width - 2, self.width - 1):
-                self.board.append(['x'] * self.width)
+                self.board.append([' '] * self.width)
             else:
                 self.board.append([' '] * self.width)
 
     def draw_board(self):
-        print('-' * (self.width * 4 + 1))
+        '''playfield drawing'''
+        # line before board
+        line = '  ' + '-' * (self.width * 4 + 1)
+        print(line)
 
-        for i in range(len(self.board)):
-            row = '| ' + ' | '.join(self.board[i]) + ' |'
+        # draw board with figures
+        for row, char in zip(range(len(self.board)), ('ABCDEFGH')):
+            print(self.board[row])
+            row = char + ' | ' + ' | '.join(self.board[row]) + ' |'
             print(row)
 
-        print('-' * (self.width * 4 + 1))
+        # line after board
+        line = '  ' + '-' * (self.width * 4 + 1)
+        print(line)
 
+        # digits after board
+        digits = '    1   2   3   4   5   6   7   8'
+        print(digits)
+
+    def insert_figure(self, figure_list: List):
+        '''add player figures to the field'''
+        pass
+
+    def shape_labels(self):
+        '''description of all the designations in the game'''
+        info = '''
+Legend
+p - pawn(пешка)
+k - knight(конь)
+b - bishop(слон)
+r - rook(ладья)
+q - queen(ферзь)
+k - king(король)
+'''
+        print(info)
